2c5d083aa303341455a464cbeecfdf123e9f8996
zhiwutian <brettalbright@outlook.com>
2018-05-12 12:00:48 -0700

    uniformed link syntax, added link to add review page from button on single results page

--- client/src/components/single_results.js
+++ client/src/components/single_results.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import {Link} from "react-router-dom";
 
 class SingleResults extends Component {
     render() {
         return (
             <div>
                 <h1>Single Results</h1>
+                <Link to="/add-review">Add Review</Link>
             </div>
         )
     }
