74233787191c6067a07b99ce1803cdec0bea20f0
zhiwutian <brettalbright@outlook.com>
2018-05-12 10:28:54 -0700

    completed initial advanced search modal and Routed to app.js

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../assets/css/app.css';
+import {Route} from "react-router-dom";
 import logo from '../assets/images/logo.svg';
+import AdvancedSearchModal from "./advanced-search-modal";
 import Test from "./test";
 
 const App = () => (
@@ -8,6 +10,7 @@ const App = () => (
         <div className="app">
             <img src={logo} className="logo rotate"/>
             <h1>Welcome to React</h1>
+            <Route path="/advanced-search" component={AdvancedSearchModal}/>
             <Test/>
         </div>
     </div>
