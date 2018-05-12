97650c530435efa522cfc4037352c13b96a69872
samskarvan <samskarvan@gmail.com>
2018-05-12 15:29:12 -0700

    successfully received data response from mysql via remote get request added credentials for dtabase connection to gitignored file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// import { INSPECT_MAX_BYTES } from "buffer";
+
 const express = require("express");
 const mysql = require('mysql');
 const cors = require("cors");
@@ -19,8 +21,25 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(resolve(__dirname, "client", "dist")));
 
-app.get("/api",(req, res) => {
-    res.send('tis working');
+app.get("/api",(req, res, next) => {
+    // res.send('tis working');
+
+    let query = 'SELECT * FROM ??';
+    let inserts =['items'];
+
+    let sql = mysql.format(query, inserts);
+
+    console.log(sql);
+
+    database.query(sql, (err,results,field)=>{
+        if(err) return next (err);
+
+        const output = {
+            succes: true,
+            data: results
+        }
+        res.json(output);
+    });
 });
 
 app.post("/api/send-data", (req, res) => {
@@ -58,6 +77,7 @@ app.listen(PORT, () => {
     console.log("Server Error:", err.message);
     console.log("Do you already have a server running on PORT:" + PORT + "?");
 });
+
 
 
 
