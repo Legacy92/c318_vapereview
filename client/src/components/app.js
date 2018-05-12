3e0f86e004898dc36ea246c4e024a4219f205454
zhiwutian <brettalbright@outlook.com>
2018-05-12 10:02:41 -0700

    initial sign-in-modal-commit

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../assets/css/app.css';
+import {Route} from "react-router-dom";
+import UserSignInModal from "./user-sign-in-modal"
 import logo from '../assets/images/logo.svg';
 import Test from "./test";
 
@@ -8,6 +10,7 @@ const App = () => (
         <div className="app">
             <img src={logo} className="logo rotate"/>
             <h1>Welcome to React</h1>
+            <Route path="/user-sign-in" component={UserSignInModal}/>
             <Test/>
         </div>
     </div>
