9eecb3359f62d11fd9c795e2b8e933af13844d8d
zhiwutian <brettalbright@outlook.com>
2018-05-12 12:24:24 -0700

    pre merge final commit, Link added to add product page and link added to single results page

--- client/src/components/add_product.js
+++ client/src/components/add_product.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import {Link} from "react-router-dom";
 
 class AddProduct extends Component {
     render() {
         return (
             <div>
                 <h1>Add Product</h1>
+                <Link to="/add-review">Add Review</Link>
             </div>
         )
     }
