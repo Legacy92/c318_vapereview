0c4f912c3dec446127768f5cc3186a372d83077f
marknemenzo <mlnemenzo@gmail.com>
2018-05-15 15:23:54 -0700

    user sign in modal completed redux forms

--- client/src/components/landing-page.js
+++ client/src/components/landing-page.js
@@ -1,15 +1,41 @@
 import React, {Component} from "react";
 import { Link } from 'react-router-dom';
+import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+
+
 class LandingPage extends Component {
     constructor(props) {
         super(props);
     }
+
+    handleLandingPageSearch(values) {
+        console.log("Landing Page Values:", values);
+
+    }
+
+    renderInput({label, input, meta: {touched, error}}) {
+        console.log(label, input);
+        return (
+            <div>
+                <label>{label}</label>
+                <input {...input} type="text" autoComplete="off"/>
+                <p className="red-text text-darken-2">{touched && error}</p>
+            </div>
+        )
+    }
+
     render(){
+
+        const {handleSubmit} = this.props;
+
         return (
             <div>
                 <h1>This is the Landing page!</h1>
-                <input type="text" placeholder = "search" />
-                <button>Go!</button>
+                <form onSubmit={handleSubmit(this.handleLandingPageSearch.bind(this))}>
+                    <Field name = "landing_page" label = "Search For Juice Here: "placeholder = "search" component = {this.renderInput} />
+                    <button>Go!</button>
+                </form>
                 <br/>
                <button><Link to = "/multiple_results">Browse</Link></button>
                <button><Link to = "/add-product">Add Juice</Link></button>
@@ -18,4 +44,22 @@ class LandingPage extends Component {
         )
     }
 }
+
+function validate({landing_page}){
+    const errors = {};
+
+    if(!landing_page) {
+        errors.landing_page = "Please enter juice query.";
+
+    }
+
+    return errors;
+}
+
+LandingPage = reduxForm({
+    form: "landing_page",
+    validate: validate
+})(LandingPage);
+
+
 export default LandingPage;
--- client/src/components/user-sign-in-modal.js
+++ client/src/components/user-sign-in-modal.js
@@ -1,4 +1,7 @@
 import React, {Component} from "react";
+import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 
 class UserSignInModal extends Component {
@@ -6,15 +9,54 @@ class UserSignInModal extends Component {
         super(props);
     }
 
-    render (){
+    handleUserSignInModal(values) {
+        console.log("Sign In Values: ", values);
+    }
+
+    renderInput({label, input, meta: {touched, error}}) {
+        console.log(label, input);
+
         return (
             <div>
-                <h1>This is the sign in Modal</h1>
+                <label>{label}</label>
+                <input {...input} type= "text" autoComplete = "off"/>
+                <p className = "red-text text-darken-2">{touched && error}</p>
+            </div>
+        )
+    }
+
+    render (){
+
+        const {handleSubmit} = this.props;
+
+
+        return (
+            <div>
+                <form onSubmit={handleSubmit(this.handleUserSignInModal.bind(this))}>
+                    <Field name = "user_signin" label = "Log In: "placeholder = "username" component = {this.renderInput} />
+                    <button>Go!</button>
+                </form>
             </div>
         )
     }
 
 
 }
+
+function validate({user_signin}){
+    const errors = {};
+
+    if(!user_signin) {
+        errors.landing_page = "Please enter juice query.";
+
+    }
+
+    return errors;
+}
+
+UserSignInModal = reduxForm({
+    form: "user_signin",
+    validate: validate
+})(UserSignInModal);
 
 export default UserSignInModal;
