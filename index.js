f1b28f5c2f368c7fd6224f7020c68bca51d98924
Dr3wbit <eightbitandrew@gmail.com>
2018-05-19 12:29:12 -0700

    pull flavor ID endpoint

--- index.js
+++ index.js
@@ -19,35 +19,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(resolve(__dirname, "client", "dist")));
 
-
-// example response
-app.get("/api/test",(req, res, next) => {
-
-    let query = 'SELECT * FROM ??';
-    let inserts =['juices'];
-
-    let sql = mysql.format(query, inserts);
-
-    console.log(sql);
-
-    database.query(sql, (err,results,field)=>{
-        if(err) return next (err);
-
-        const output = {
-            success: true,
-            data: results
-        }
-        res.json(output);
-    });
-});
-
 //Get Data by Flavor ID
 app.get("/api/multiple-results",(req, res, next) => {
     let { flavor } = req.body;
     flavor = 1;
 
     let query = 'SELECT * FROM ?? JOIN ?? ON ?? = ?? JOIN ?? ON ?? = ?? WHERE ?? = ?';
-    let inserts =['juices-flavors', 'review', 'juices-flavors.review_id', 'review.id', 'juices', 'review.juice_id', 'juices.id', 'juices-flavors.flavor_id', flavor];
+    let inserts =['juices-flavors', 'reviews', 'juices-flavors.review_id', 'reviews.id', 'juices', 'reviews.juice_id', 'juices.id', 'juices-flavors.flavor_id', flavor];
 
     let sql = mysql.format(query, inserts);
 
@@ -70,7 +48,7 @@ app.get("/api/single-results",(req, res, next) => {
     singleJuice = 3;
 
     let query = 'SELECT * FROM ?? JOIN ?? ON ?? = ?? WHERE ?? = ?';
-    let inserts =['juices','review','juices.id','review.juice_id', 'juices.id', singleJuice];
+    let inserts =['juices','reviews','juices.id','reviews.juice_id', 'juices.id', singleJuice];
 
     let sql = mysql.format(query, inserts);
 
@@ -91,9 +69,9 @@ app.get("/api/single-results",(req, res, next) => {
 app.get("/api/single-results",(req, res, next) => {
     let { flavors } = req.body;
     flavors = 3;
-// SELECT * from `review` JOIN `juices-flavors` ON `juices-flavors`.`review_id` = `review`.`juice_id` WHERE `review`.`juice_id` = 3
+// SELECT * from `reviews` JOIN `juices-flavors` ON `juices-flavors`.`reviews_id` = `reviews`.`juice_id` WHERE `reviews`.`juice_id` = 3
     let query = 'SELECT * FROM ?? JOIN ?? ON ?? = ?? WHERE ?? = ?';
-    let inserts =['juices','review','juices.id','review.juice_id', 'juices.id', flavors];
+    let inserts =['juices','reviews','juices.id','reviews.juice_id', 'juices.id', flavors];
 
     let sql = mysql.format(query, inserts);
 
@@ -154,7 +132,7 @@ app.post('/api/add-review', (req,res,next)=>{
     const {rating, description, juice_id, user_id} = req.body;
 
     let query = 'INSERT INTO ?? (??, ??, ??, ??, ??) VALUES (?, ?, ?, ?, ?)';
-    let inserts = ['review', 'id', 'rating', 'description', 'juice_id', 'user_id', 'NULL', rating, description, juice_id, user_id];
+    let inserts = ['reviews', 'id', 'rating', 'description', 'juice_id', 'user_id', 'NULL', rating, description, juice_id, user_id];
 
     let sql = mysql.format(query, inserts);
     console.log("This is the formatted SQL", sql);
@@ -190,7 +168,7 @@ app.get('/api/log-in', (req,res,next)=>{
 
 
 //Get Flavor ID's
-app.get("/api/*****",(req, res, next) => {
+app.get("/api/test",(req, res, next) => {
     let { singleJuice } = req.body;
     singleJuice = 3;
 
@@ -227,7 +205,3 @@ app.listen(PORT, () => {
     console.log("Server Error:", err.message);
     console.log("Do you already have a server running on PORT:" + PORT + "?");
 });
-
-
-
-
