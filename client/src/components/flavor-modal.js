197e3f63b5adc1528e78d315160cc0e7d0ae178c
Dr3wbit <eightbitandrew@gmail.com>
2018-05-21 14:21:42 -0700

    added endpoints for the flavor modal page

--- client/src/components/flavor-modal.js
+++ client/src/components/flavor-modal.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
 class FlavorModal extends Component {
+
+
+    componentDidMount(){
+        // flavors needs the category ID
+        // this.flavors();
+        this.categorys();
+    }
+
+    // send id
+    async flavors(){
+        const response = await axios.get("/api/flavor-modal");
+        console.log("flavors:", response);
+    }
+
+    // this is fine
+    async categorys(){
+        const response = await axios.get("/api/category-modal");
+        console.log("categorys:", response);
+    }
+
+
     render() {
         return (
             <div>
