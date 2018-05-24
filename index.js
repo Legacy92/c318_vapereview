ef72cc3bbeb7670a9248db2a4502bf04dc35298f
samskarvan <samskarvan@gmail.com>
2018-05-23 17:23:28 -0700

    cleaned up some backend code

--- index.js
+++ index.js
@@ -19,57 +19,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(resolve(__dirname, "client", "dist")));
 
-// //Get Data by Flavor ID
-// app.get("/api/multiple-results", (req, res, next) => {
-//     let { flavor } = req.body;
-//     flavor = 1;
 
-//     let query = 'SELECT * FROM ?? JOIN ?? ON ?? = ?? JOIN ?? ON ?? = ?? WHERE ?? = ?';
-//     let inserts = ['juices-flavors', 'reviews', 'juices-flavors.review_id', 'reviews.id', 'juices', 'reviews.juice_id', 'juices.id', 'juices-flavors.flavor_id', flavor];
-
-//     let sql = mysql.format(query, inserts);
-
-//     console.log(sql);
-
-//     database.query(sql, (err, results, field) => {
-//         if (err) return next(err);
-
-//         const output = {
-//             success: true,
-//             data: results
-//         }
-//         res.json(output);
-//     });
-// });
-
-// Get Data by Flavor Name
-// app.get("/api/multiple-results", (req, res, next) => {
-//     let { flavor } = req.body;
-
-//     let query = 'SELECT * FROM ?? JOIN ?? ON ?? = ?? JOIN ?? ON ?? = ?? JOIN ?? ON ?? = ?? WHERE ?? = ?';
-//     let inserts = ['`flavors`', 'juices-flavors` b', '`b`.`flavor_id`', '`f`.`id`', '`reviews` r', '`r`.`id`', '`b`.`review_id`', '`juices` j', '`j`.`id`', '`r`.`juice_id`', ' `f`.`flavor`', flavor];
-
-//     let sql = mysql.format(query, inserts);
-
-//     console.log(sql);
-
-//     database.query(sql, (err, results, field) => {
-//         if (err) return next(err);
-
-//         const output = {
-//             success: true,
-//             data: results
-//         }
-//         res.json(output);
-//     });
-// });
-
-// Browse 
+// Browse button on landing page - returns all juices
 app.get("/api/multiple-results-browse", (req, res, next) => {
-    let query = 'SELECT * FROM `juices` LEFT JOIN `reviews` ON `juices`.`id` = `reviews`.`juice_id`';
-    // let inserts = ['`juices`', 'juices-flavors` b', '`b`.`flavor_id`', '`f`.`id`', '`reviews` r', '`r`.`id`', '`b`.`review_id`', '`juices` j', '`j`.`id`', '`r`.`juice_id`', ' `f`.`flavor`', flavor];
-
-    let sql = mysql.format(query);
+    const query = 'SELECT * FROM `juices` LEFT JOIN `reviews` ON `juices`.`id` = `reviews`.`juice_id`';
+    const sql = mysql.format(query);
 
     console.log(sql);
 
@@ -84,10 +38,10 @@ app.get("/api/multiple-results-browse", (req, res, next) => {
     });
 });
 
-//get random juice
+// Random juice button on landing page - gets single random juice 
 app.get("/api/random-juice",(req, res, next) => {
-    let query = 'SELECT * FROM `juices` ORDER BY RAND() LIMIT 1';
-    let sql = mysql.format(query);
+    const query = 'SELECT * FROM `juices` ORDER BY RAND() LIMIT 1';
+    const sql = mysql.format(query);
 
     console.log(sql);
 
@@ -102,38 +56,14 @@ app.get("/api/random-juice",(req, res, next) => {
     });
 });
 
-//search by all 
+//  Landing page search by query - allows for search by juice name, manufactuer name, flavor name or flavor category 
 app.get("/api/multiple-results", (req, res, next) => {
-    let { input } = req.query;
+    const { input } = req.query;
 
+    const query = 'SELECT `j`.*, `r`.`rating`,`r`.`id` AS review_id, `c`.`category`, `f`.`flavor` from `juices` j LEFT JOIN `reviews` r ON `j`.`id` = `r`.`juice_id` LEFT JOIN `juices-flavors` ON `juices-flavors`.`review_id` = `r`.`id` LEFT JOIN `flavors` as f ON `f`.`id` = `juices-flavors`.`flavor_id` LEFT JOIN `category` as c ON `c`.`id` = `f`.`catagory_id` WHERE `j`.`name` = ? OR `j`.`manufacturer_name` = ? OR `f`.`flavor` = ? OR `c`.`category` = ?';
+    const inserts = [input, input, input, input] ;
 
-    let query = 'SELECT `j`.*, `r`.`rating`,`r`.`id` AS review_id, `c`.`category`, `f`.`flavor` from `juices` j LEFT JOIN `reviews` r ON `j`.`id` = `r`.`juice_id` LEFT JOIN `juices-flavors` ON `juices-flavors`.`review_id` = `r`.`id` LEFT JOIN `flavors` as f ON `f`.`id` = `juices-flavors`.`flavor_id` LEFT JOIN `category` as c ON `c`.`id` = `f`.`catagory_id` WHERE `j`.`name` = ? OR `j`.`manufacturer_name` = ? OR `f`.`flavor` = ? OR `c`.`category` = ?';
-    let inserts = [input, input, input, input] ;
-
-    let sql = mysql.format(query, inserts);
-
-    console.log(sql);
-
-    database.query(sql, (err, results, field) => {
-        if (err) return next(err);
-
-        const output = {
-            success: true,
-            data: results
-        }
-        res.json(output);
-    });
-});
-
-//Get Single Juice Results
-app.get("/api/single-results", (req, res, next) => {
-    let { singleJuice } = req.body;
-    singleJuice = 3;
-
-    let query = 'SELECT * FROM ?? JOIN ?? ON ?? = ?? WHERE ?? = ?';
-    let inserts = ['juices', 'reviews', 'juices.id', 'reviews.juice_id', 'juices.id', singleJuice];
-
-    let sql = mysql.format(query, inserts);
+    const sql = mysql.format(query, inserts);
 
     console.log(sql);
 
@@ -148,15 +78,15 @@ app.get("/api/single-results", (req, res, next) => {
     });
 });
 
-//Get Flavors for Chart by juice_id
-app.get("/api/single-results", (req, res, next) => {
-    let { flavors } = req.body;
-    flavors = 3;
-    // SELECT * from `reviews` JOIN `juices-flavors` ON `juices-flavors`.`reviews_id` = `reviews`.`juice_id` WHERE `reviews`.`juice_id` = 3
-    let query = 'SELECT * FROM ?? JOIN ?? ON ?? = ?? WHERE ?? = ?';
-    let inserts = ['juices', 'reviews', 'juices.id', 'reviews.juice_id', 'juices.id', flavors];
 
-    let sql = mysql.format(query, inserts);
+// Flavor chart on single results page - gets flavor breakdown of each juice
+app.get("/api/flavor-chart", (req, res, next) => {
+    const { juice_id } = req.body;
+    
+    const query = 'SELECT * FROM ?? JOIN ?? ON ?? = ?? WHERE ?? = ?';
+    const inserts = ['juices', 'reviews', 'juices.id', 'reviews.juice_id', 'juices.id', juice_id];
+
+    const sql = mysql.format(query, inserts);
 
     console.log(sql);
 
@@ -176,10 +106,10 @@ app.get("/api/single-results", (req, res, next) => {
 app.post("/api/create-user", (req, res, next) => {
     const { username, password } = req.body;
 
-    let query = 'INSERT INTO ?? (??, ??, ??, ??, ??) VALUES (?, ?, ?, ?, ?)';
-    let inserts = ['users', 'username', 'password', 'email', 'created', 'role', username, password, 'NULL', 'CURRENT_TIMESTAMP', '1'];
+    const query = 'INSERT INTO ?? (??, ??, ??, ??, ??) VALUES (?, ?, ?, ?, ?)';
+    const inserts = ['users', 'username', 'password', 'email', 'created', 'role', username, password, 'NULL', 'CURRENT_TIMESTAMP', '1'];
 
-    let sql = mysql.format(query, inserts);
+    const sql = mysql.format(query, inserts);
     console.log("This is the formatted SQL", sql);
     database.query(sql, (err, results, fields) => {
         if (err) return next(err);
@@ -195,10 +125,10 @@ app.post("/api/create-user", (req, res, next) => {
 app.post('/api/add-product', (req, res, next) => {
     const { name, manufacturer, description, site } = req.body;
 
-    let query = 'INSERT INTO ?? (??, ??, ??, ??) VALUES ( ?, ?, ?, ?)';
-    let inserts = ['juices', 'name', 'manufacturer_name', 'manufacturer_site', 'manufacturer_description', name, manufacturer, site, description ];
+    const query = 'INSERT INTO ?? (??, ??, ??, ??) VALUES ( ?, ?, ?, ?)';
+    const inserts = ['juices', 'name', 'manufacturer_name', 'manufacturer_site', 'manufacturer_description', name, manufacturer, site, description ];
 
-    let sql = mysql.format(query, inserts);
+    const sql = mysql.format(query, inserts);
     console.log("This is the formatted SQL", sql);
     database.query(sql, (err, results, fields) => {
         if (err) return next(err);
@@ -210,7 +140,7 @@ app.post('/api/add-product', (req, res, next) => {
     })
 });
 
-//add review
+//add review - !important!!!!!!
 
 app.post('/api/add-review', (req, res, next) => {
     const { rating, description, juice_id, user_id, flavor } = req.body;
